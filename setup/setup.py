ece69d14e3f1b14db14a1035ee810ef44ac3b992
jahnito <jahnito@gmail.com>
2025-02-02 19:38:00 +0500

    Добавлен функионал инициализации БД

--- setup/setup.py
+++ setup/setup.py
@@ -1,3 +1,20 @@
 import sqlite3
+import pathlib
 
 
+SETUP_PATH = pathlib.Path(__file__).parent.parent
+DB_PATH = pathlib.Path(str(SETUP_PATH) + '/database.db')
+DB_SCRIPT = pathlib.Path(str(SETUP_PATH) + '/database.sql')
+
+
+if DB_PATH.exists():
+    print(f'Файл базы данных существует\n{DB_PATH}')
+else:
+    try:
+        scr = open(DB_SCRIPT, 'r')
+        with sqlite3.connect(DB_PATH) as conn:
+            conn.executescript(scr.read())
+            conn.commit()        
+        scr.close()
+    except sqlite3.Error as e:
+        print(e)
