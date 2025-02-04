d2d4f26ee2ff5026777def7c38bc36ded45763c8
jahnito <jahnito@gmail.com>
2025-02-05 00:18:18 +0500

    Добавлены функции генерации регулярок и параметров фильтрации слов

--- functions/functions.py
+++ functions/functions.py
@@ -2,7 +2,12 @@ import re
 from itertools import repeat
 
 
+from pprint import pprint
+
 def show_pos_letters(line: str) -> str:
+    '''
+    Функция формирует строку из позиций и букв стоящих на указанных позициях
+    '''
     if not line:
         return ''
     res = {}
@@ -18,6 +23,9 @@ def show_pos_letters(line: str) -> str:
 
 
 def filter_regex(line, regex) -> bool:
+    '''
+    Функция ищет совпадение по регулярному выражению
+    '''
     if re.match(regex, line):
         return True
     else:
@@ -25,151 +33,235 @@ def filter_regex(line, regex) -> bool:
 
 
 def words_filter(dictionary: list, params: dict) -> list:
+    '''
+    Функция фильтрации слов по параметрам
+    '''
     for i in params:
         dictionary = list(filter(lambda x: filter_regex(x, params[i]), dictionary))
-        # dictionary = list(filter(filter_regex, zip(dictionary, repeat(params[i]))))
-        # print(params[i])
-    # print(dictionary)
     dictionary.sort()
     return dictionary
 
 
-    # dictionary = filter(filter_regex, zip(dictionary, repeat()))
+def set_exclude_regex(line: str) -> str:
+    '''
+    Функция формирует регулярку для исключаемых букв
+    '''
+    if line:
+        head_line = '^((?!['
+        tail_line = '])\w)*$'
+        return f'{head_line}{line}{tail_line}'
+    else:
+        return False
 
 
-if __name__ == '__main__':
-    params = {
-              'ex': r'^((?![а])\w)*$',
-              'in': r'^\w*[ё]{1}\w*$',
-              'np': r'\w\w\w[^лт]\w',
-              'ip': r'\w\w\w[уо]\w'
-              }
+def set_include_regex(line: str) -> str:
+    '''
+    Функция формирует регулярку для входящих букв
+    '''
+    if line:
+        head_line = '^\w*['
+        tail_line = ']{1}\w*$'
+        return f'{head_line}{line}{tail_line}'
+    else:
+        return False
 
 
-    words = [
-        'ррарр',
-        'рррра',
-        'арррр',
-        'ясное',
- 'ясной',
- 'ясном',
- 'ясною',
- 'ясную',
- 'ясные',
- 'ясный',
- 'ясным',
- 'ясных',
- 'яспис',
- 'яссах',
- 'яства',
- 'ястве',
- 'яство',
- 'яству',
- 'ястык',
- 'ясырь',
- 'ятвяг',
- 'ятках',
- 'яткой',
- 'яткою',
- 'ятных',
- 'ятовь',
- 'яться',
- 'ятями',
- 'яффой',
- 'яхонт',
- 'яхтам',
- 'яхтах',
- 'яхтой',
- 'яхтою',
- 'ячать',
- 'ячеей',
- 'ячеек',
- 'ячеею',
- 'ячеям',
- 'ячеях',
- 'ячеёй',
- 'ячеёю',
- 'ячная',
- 'ячное',
- 'ячной',
- 'ячном',
- 'ячною',
- 'ячную',
- 'ячные',
- 'ячный',
- 'ячным',
- 'ячных',
- 'ячьей',
- 'ячьем',
- 'ячьим',
- 'ячьих',
- 'яшмой',
- 'яшмою',
- 'ящера',
- 'ящере',
- 'ящеру',
- 'ящеры',
- 'ящика',
- 'ящике',
- 'ящики',
- 'ящику',
- 'ящура',
- 'ящуре',
- 'ящуру',
- 'ёжась',
- 'ёжика',
- 'ёжике',
- 'ёжики',
- 'ёжику',
- 'ёжила',
- 'ёжили',
- 'ёжило',
- 'ёжите',
- 'ёжить',
- 'ёжишь',
- 'ёжусь',
- 'ёжься',
- 'ёжьте',
- 'ёкаем',
- 'ёкает',
- 'ёкала',
- 'ёкали',
- 'ёкало',
- 'ёкать',
- 'ёкают',
- 'ёкнем',
- 'ёкнет',
- 'ёкнув',
- 'ёкнул',
- 'ёкнут',
- 'ёлкам',
- 'ёлках',
- 'ёлкой',
- 'ёлкою',
- 'ёмкая',
- 'ёмкие',
- 'ёмкий',
- 'ёмким',
- 'ёмких',
- 'ёмкое',
- 'ёмкой',
- 'ёмком',
- 'ёмкою',
- 'ёмкую',
- 'ёрзай',
- 'ёрзал',
- 'ёрзаю',
- 'ёрзая',
- 'ёрник',]
+def set_position_dict(line: str) -> dict:
+    res = {}
+    if not line:
+        return {}
+    for i in line.split(':'):
+        if int(i[0]) in res:
+            res[int(i[0])].append(i[1])
+        else:
+            res[int(i[0])] = [i[1]]
+    return res
 
-    words.sort()
+
+def set_np_regex(line: str, length: int):
+    '''
+    Функция формирует регулярку для букв находящихся не на своих позициях
+    '''
+    res = []
+    empty_letter = '\w'
+    positons = set_position_dict(line)
+    head_line='[^'
+    tail_line=']'
+    for i in range(1, length + 1):
+        if i in positons:
+            res.append(f'{head_line}{"".join(positons[i])}{tail_line}')
+        else:
+            res.append(empty_letter)
+    return ''.join(res)
+
+
+def set_ip_regex(line: str, length: int):
+    '''
+    Функция формирует регулярку для букв находящихся не на своих позициях
+    '''
+    res = []
+    empty_letter = '\w'
+    positons = set_position_dict(line)
+    head_line='['
+    tail_line=']'
+    for i in range(1, length + 1):
+        if i in positons:
+            res.append(f'{head_line}{"".join(positons[i])}{tail_line}')
+        else:
+            res.append(empty_letter)
+    return ''.join(res)
+
+
+def gen_params(exc_line: str, inc_line: str, np_line: str, ip_line: str, length: int) -> list:
+    params = {}
+    if line:=set_exclude_regex(exc_line):
+        params['ex'] = line
+    if line:=set_include_regex(inc_line):
+        params['in'] = line
+    if line:=set_np_regex(np_line, length):
+        params['np'] = line
+    if line:=set_ip_regex(ip_line, length):
+        params['ip'] = line
+    return params
+
+
+# if __name__ == '__main__':
+    # ex = 
+    # np = '4л:4т'
+    # ip = '4у:4о'
+
+    # pprint(gen_position_dict(np))
+
+
+
+
+
+#     pprint(params)
+
+#     words = [
+#         'ррарр',
+#         'рррра',
+#         'арррр',
+#         'ясное',
+#  'ясной',
+#  'ясном',
+#  'ясною',
+#  'ясную',
+#  'ясные',
+#  'ясный',
+#  'ясным',
+#  'ясных',
+#  'яспис',
+#  'яссах',
+#  'яства',
+#  'ястве',
+#  'яство',
+#  'яству',
+#  'ястык',
+#  'ясырь',
+#  'ятвяг',
+#  'ятках',
+#  'яткой',
+#  'яткою',
+#  'ятных',
+#  'ятовь',
+#  'яться',
+#  'ятями',
+#  'яффой',
+#  'яхонт',
+#  'яхтам',
+#  'яхтах',
+#  'яхтой',
+#  'яхтою',
+#  'ячать',
+#  'ячеей',
+#  'ячеек',
+#  'ячеею',
+#  'ячеям',
+#  'ячеях',
+#  'ячеёй',
+#  'ячеёю',
+#  'ячная',
+#  'ячное',
+#  'ячной',
+#  'ячном',
+#  'ячною',
+#  'ячную',
+#  'ячные',
+#  'ячный',
+#  'ячным',
+#  'ячных',
+#  'ячьей',
+#  'ячьем',
+#  'ячьим',
+#  'ячьих',
+#  'яшмой',
+#  'яшмою',
+#  'ящера',
+#  'ящере',
+#  'ящеру',
+#  'ящеры',
+#  'ящика',
+#  'ящике',
+#  'ящики',
+#  'ящику',
+#  'ящура',
+#  'ящуре',
+#  'ящуру',
+#  'ёжась',
+#  'ёжика',
+#  'ёжике',
+#  'ёжики',
+#  'ёжику',
+#  'ёжила',
+#  'ёжили',
+#  'ёжило',
+#  'ёжите',
+#  'ёжить',
+#  'ёжишь',
+#  'ёжусь',
+#  'ёжься',
+#  'ёжьте',
+#  'ёкаем',
+#  'ёкает',
+#  'ёкала',
+#  'ёкали',
+#  'ёкало',
+#  'ёкать',
+#  'ёкают',
+#  'ёкнем',
+#  'ёкнет',
+#  'ёкнув',
+#  'ёкнул',
+#  'ёкнут',
+#  'ёлкам',
+#  'ёлках',
+#  'ёлкой',
+#  'ёлкою',
+#  'ёмкая',
+#  'ёмкие',
+#  'ёмкий',
+#  'ёмким',
+#  'ёмких',
+#  'ёмкое',
+#  'ёмкой',
+#  'ёмком',
+#  'ёмкою',
+#  'ёмкую',
+#  'ёрзай',
+#  'ёрзал',
+#  'ёрзаю',
+#  'ёрзая',
+#  'ёрник',]
+
+#     words.sort()
 
 
     # print(*words, sep='\n')
 
-    print(*words)
+    # print(*words)
 
 
-    print('\n\n')
+    # print('\n\n')
 
-    print(*words_filter(words, params))
+    # print(*words_filter(words, params))
